7beff302610a8333563b4f65e1e99c21a2e26bc8
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2022-11-20 21:21:48 +0100

    Refactor code & add data model

--- WPFCppLab3/DataModel.h
+++ WPFCppLab3/DataModel.h
@@ -1,34 +1,77 @@
-#include <windows.h>
+#pragma once
+
 using namespace System::Reflection;
+using namespace System::Runtime::Remoting;
 using namespace System::Security;
 
-typedef System::Collections::ObjectModel::ObservableCollection<MethodInfo^> myList;
+ref class DataModel sealed
+{
+	SecurityZone _selectedSecurityZone = SecurityZone::MyComputer;
 
-ref class DataModel {
-	SecurityZone selectedSecurityZone = System::Security::SecurityZone::MyComputer;
-	myList^ methods = gcnew myList;
-	// tutaj zdefiniuj pozosta³e zmienne modelu danych
+	array<MethodInfo^>^ _methods = nullptr;
+	array<FieldInfo^>^ _fields = nullptr;
+	Object^ _instance;
+    System::Type^ _type;
+	ObjectHandle^ _handle;
 
 public:
 	property SecurityZone SelectedSecurityZone {
 		SecurityZone get() {
-			return selectedSecurityZone;
+			return _selectedSecurityZone;
 		}
 
-		void set(SecurityZone value) {
-			selectedSecurityZone = value;
-		}
-	}
-	property myList^ Methods {
-		myList^ get() {
-			return methods;
-		}
-
-		void set(myList^ value) {
-			methods = value;
+		void set(const SecurityZone value) {
+			_selectedSecurityZone = value;
 		}
 	}
 
-	// tutaj zdefiniuj pozosta³e w³aœciwiœci modelu danych
+	property array<MethodInfo^>^ Methods {
+		array<MethodInfo^>^ get() {
+			return _methods;
+		}
 
+		void set(array<MethodInfo^>^ value) {
+			_methods = value;
+		}
+	}
+
+	property array<FieldInfo^>^ Fields {
+		array<FieldInfo^>^ get() {
+			return _fields;
+		}
+
+		void set(array<FieldInfo^>^ value) {
+			_fields = value;
+		}
+	}
+
+	property Object^ Instance {
+		Object^ get() {
+			return _instance;
+		}
+
+		void set(Object^ value) {
+			_instance = value;
+		}
+	}
+
+	property ObjectHandle^ Handle {
+		ObjectHandle^ get() {
+			return _handle;
+		}
+
+		void set(ObjectHandle^ value) {
+			_handle = value;
+		}
+	}
+
+	property System::Type^ Type {
+		System::Type^ get() {
+			return _type;
+		}
+
+		void set(System::Type^ value) {
+			_type = value;
+		}
+	}
 };
--- WPFCppLab3/Main.cpp
+++ WPFCppLab3/Main.cpp
@@ -1,105 +1,100 @@
 #include <windows.h>
 #include "DataModel.h"
 using namespace System::Runtime::Remoting;
-using namespace System::Security::Policy;
+using namespace Policy;
 using namespace System::Security;
 using namespace System;
-using namespace System::Windows;
-using namespace System::IO;
-using namespace System::Windows::Markup;
-using namespace System::Windows::Controls;
-using namespace System::CodeDom;
-using namespace System::CodeDom::Compiler;
-using namespace System::Reflection;
-using namespace System::Windows::Documents;
+using namespace Windows;
+using namespace IO;
+using namespace Markup;
+using namespace Controls;
+using namespace CodeDom;
+using namespace Compiler;
+using namespace Reflection;
+using namespace Documents;
 using namespace std;
 
 
-public ref class MyApplication : public Application
+public ref class MyApplication sealed : public Application
 {
+	DataModel ^_dataModel;
+	String ^_pathToCode, ^_code;
+	Button ^_addButton, ^_invokeButton, ^_setButton;
+	RichTextBox ^_richTextBox;
+	ListBox ^_errorsListBox, ^_methodsListBox, ^_variablesListBox;
+	Assembly ^_assembly;
+	Type ^_type;
+	Object ^_instance;
+	ObjectHandle ^_handle;
+	array<FieldInfo^> ^_fields;
+	MethodInfo ^_methodInfo;
+	FieldInfo ^_fieldInfo;
+	TextBox ^_resultTextBox, ^_parameterTextBox, ^_setTextBox, ^_setTextBox2;
+	ComboBox ^_checkZoneComboBox;
+
+	Void OnSelectionMethodsListBoxChanged(Object^ sender, SelectionChangedEventArgs^ e)
+	{
+
+	}
+	
+	Void OnSelectionVariablesListBoxChanged(Object^ sender, SelectionChangedEventArgs^ e)
+	{
+
+	}
+
+	Void OnInvokeButtonClick(Object^ sender, RoutedEventArgs^ e)
+	{
+
+	}
+
+	Void OnSetButtonClick(Object^ sender, RoutedEventArgs^ e)
+	{
+
+	}
+	
+	Void OnAddButtonClick(Object^ sender, RoutedEventArgs^ e)
+	{
+
+	}
+
 public:
-
-private: DataModel^ h_dm;
-private: TextRange ^textRang;
-private: String ^ pathToVBCode, ^code, ^tekst, ^st;
-private: Button ^addBtn, ^invokeBtn, ^setBtn;
-private: RichTextBox ^codeRTB;
-private: ListBox ^errorsListBox, ^methodsListBox, ^variablesListBox;
-private: Assembly^ modul;
-private: Type^ t;
-private: Object^ obj;
-private: ObjectHandle^ handle;
-private: array<FieldInfo^>^ fields;
-private: MethodInfo^ mI;
-private: FieldInfo^ fI;
-private: TextBox ^resultTextBox, ^parameterTextBox, ^setTextBox, ^setTextBox2;
-private: ComboBox ^checkZoneComboBox;
-
-	public: MyApplication(Window ^win)
+	MyApplication(Window ^win)
 	{
-		h_dm = gcnew DataModel();
-		win->DataContext = h_dm;
+		_dataModel = gcnew DataModel();
+		win->DataContext = _dataModel;
 
-		pathToVBCode = gcnew String("..\\VBCode\\Class1.vb");
-		code = File::ReadAllText(pathToVBCode);
-		addBtn = (Button^)win->FindName("addButton");
-		addBtn->Click += gcnew RoutedEventHandler(this, &MyApplication::OnAddBtnClick);
-		invokeBtn = (Button^)win->FindName("invokeButton");
-		invokeBtn->Click += gcnew RoutedEventHandler(this, &MyApplication::OninvokeBtnClick);
-		setBtn = (Button^)win->FindName("setButton");
-		setBtn->Click += gcnew RoutedEventHandler(this, &MyApplication::OnsetBtnClick);
-		methodsListBox = (ListBox^)win->FindName("methodsListBox");
-		methodsListBox->SelectionChanged += gcnew System::Windows::Controls::SelectionChangedEventHandler(this, &MyApplication::OnSelectionMethodsListBoxChanged);
-		variablesListBox = (ListBox^)win->FindName("variablesListBox");
-		variablesListBox->SelectionChanged += gcnew System::Windows::Controls::SelectionChangedEventHandler(this, &MyApplication::OnSelectionVariablesListBoxChanged);
-		errorsListBox = (ListBox^)win->FindName("errorsListBox");
-		methodsListBox = (ListBox^)win->FindName("methodsListBox");
-		variablesListBox = (ListBox^)win->FindName("variablesListBox");
-		resultTextBox = (TextBox^)win->FindName("resultTextBox");
-		parameterTextBox = (TextBox^)win->FindName("parameterTextBox");
-		setTextBox = (TextBox^)win->FindName("setTextBox");
-		setTextBox2 = (TextBox^)win->FindName("setTextBox2");
-		checkZoneComboBox = (ComboBox^)win->FindName("checkZoneComboBox");
-		codeRTB = (RichTextBox^)win->FindName("codeRichTextBox");
-		codeRTB->AppendText(code);
-		codeRTB->Document->LineHeight = 0.1;
+		_pathToCode = gcnew String("..\\VBCode\\Class1.vb");
+		_code = File::ReadAllText(_pathToCode);
+
+		_addButton =		 safe_cast<Button^>(win->FindName("addButton"));
+		_invokeButton =		 safe_cast<Button^>(win->FindName("invokeButton"));
+		_setButton =		 safe_cast<Button^>(win->FindName("setButton"));
+		_methodsListBox =	 safe_cast<ListBox^>(win->FindName("methodsListBox"));
+		_variablesListBox =  safe_cast<ListBox^>(win->FindName("variablesListBox"));
+		_errorsListBox =	 safe_cast<ListBox^>(win->FindName("errorsListBox"));
+		_resultTextBox =	 safe_cast<TextBox^>(win->FindName("resultTextBox"));
+		_parameterTextBox =  safe_cast<TextBox^>(win->FindName("parameterTextBox"));
+		_setTextBox =		 safe_cast<TextBox^>(win->FindName("setTextBox"));
+		_setTextBox2 =		 safe_cast<TextBox^>(win->FindName("setTextBox2"));
+		_checkZoneComboBox = safe_cast<ComboBox^>(win->FindName("checkZoneComboBox"));
+
+		_addButton->Click		+= gcnew RoutedEventHandler(this, &MyApplication::OnAddButtonClick);
+		_invokeButton->Click	+= gcnew RoutedEventHandler(this, &MyApplication::OnInvokeButtonClick);
+		_setButton->Click		+= gcnew RoutedEventHandler(this, &MyApplication::OnSetButtonClick);
+		_methodsListBox->SelectionChanged +=	gcnew SelectionChangedEventHandler(this, &MyApplication::OnSelectionMethodsListBoxChanged);
+		_variablesListBox->SelectionChanged +=	gcnew SelectionChangedEventHandler(this, &MyApplication::OnSelectionVariablesListBoxChanged);
+
+		_richTextBox = safe_cast<RichTextBox^>(win->FindName("codeRichTextBox"));
+		_richTextBox->AppendText(_code);
+		_richTextBox->Document->LineHeight = 0.1;
 	}
-
-	private: System::Void OnSelectionMethodsListBoxChanged(Object^ sender, SelectionChangedEventArgs^ e)
-	{
-
-	}
-
-	private: System::Void OnSelectionVariablesListBoxChanged(Object^ sender, SelectionChangedEventArgs^ e)
-	{
-
-	}
-	private: System::Void OninvokeBtnClick(Object^ sender, RoutedEventArgs^ e)
-	{
-
-	}
-
-
-	private: System::Void OnsetBtnClick(Object^ sender, RoutedEventArgs^ e)
-	{
-
-	}
-
-	private: System::Void OnAddBtnClick(Object^ sender, RoutedEventArgs^ e)
-	{
-
-	}
-
 };
 
 [STAThread]
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-	LPSTR lpCmd, int nCmd)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
 	Stream^ st = File::OpenRead("MainWindow.xaml");
-	Window^ win = (Window^)XamlReader::Load(st, nullptr);
+	Window^ win = safe_cast<Window^>(XamlReader::Load(st, nullptr));
 	Application^ app = gcnew MyApplication(win);
 	app->Run(win);
 }
-
-
